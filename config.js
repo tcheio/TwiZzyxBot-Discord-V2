5af843beba2232ca33f40d1c1d43dfc79d9ffc42
tcheio <113595151+tcheio@users.noreply.github.com>
2023-03-15 22:22:08 +0100

    encore test gitignore

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     clients: {
-        token: 'TOKEN',
+        token: 'MTAyMjQyMjg1NDYzMDkwMzg2OA.GmEOQh.alfBBUxeqb2ETyNnGXGsX_6E92xJU2kML5mtE0',
         name: 'TwiZzyx Bot',
         logo: 'https://zupimages.net/up/22/49/c838.png',
     }
