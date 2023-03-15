bc4c05acd8526ca9ace42751981ca1c105d165e4
tcheio <113595151+tcheio@users.noreply.github.com>
2023-03-15 22:18:46 +0100

    Ajout config en ignore

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     clients: {
-        token: 'TOKEN',
+        token: 'MTAyMjQyMjg1NDYzMDkwMzg2OA.G7vfk7.lT6_5LRSSwihlUPxTKCoEwCfzd71M6pxfltId4',
         name: 'TwiZzyx Bot',
         logo: 'https://zupimages.net/up/22/49/c838.png',
     }
