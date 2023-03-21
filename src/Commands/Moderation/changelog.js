205bb424fccea0eade22b3e9328d0d6ddef49d90
tcheio <thomas.cheio@lyceedecachan-sti.fr>
2023-03-21 16:02:43 +0100

    Changelog

--- src/Commands/Moderation/changelog.js
+++ src/Commands/Moderation/changelog.js
@@ -1,16 +1,11 @@
 const { EmbedBuilder, PermissionsBitField } = require('discord.js');
 const config = require('../../../config');
 
-const msg="- <:Twitch:748225816973803562>**NOUVELLES ANNONCES DE STREAM** qui s'adapte automatiquement à ce qu'on fait (*Ça dépend du scenario et bon vous les avez déjà vu depuis un moment*) \n"+ 
-          "- **Ajout des permissions pour les commandes**, vous ne pouvez utiliser qu'une partie des commandes et le staffs peuvent toutes les utilisés\n"+
-          "- **Ajout de la première commande pour vous:** `/planning` qui comme son nom l'indique vous affiche le planning de la semaine en cours. J'en profiterai également pour mettre à jour le planning photo dés que je suis au courant donc en cas de changement de programme vous pourrez le voir de manière visible sur une image planning"+
-          "- **Ajout de la commande** `/clear` **pour __le staff__** qui permet de clear un certains nombre de messages et même choisir les utilisateurs\n"+
-          "- **Ajout de la commande** `/planningreset` **pour __le staff__** qui permet de reset le planning en fin de semaine (*Commande provisoire le temps d'automatiser le proces*)\n\n"+
-          
-          "Outre ça, il y a également eu **une optimisation de certaines parcels de code**, **une optimisation également de la gestions des annonces** qui est désormais gérer sur un serveur externe, pleins **d'ajouts de gif pour les annonces de streams** que vous avez commencer à voir et que je mettrais à jour régulièrement.\n\n"+
-          "**L'hébergeur a également été améliorer en espérant moins de crash innopiné** (*Je croise les doigts*).\n\n"+
-          
-          "**J'espère que toutes ses nouveautés vont vous plaires**, si vous voyez un **bug** et une **suggestion** pour le bot, n'hésitez pas à le faire dans le <#1060679452146802769>";
+const msg="- **Fix des perms commandes** (Le staff peut enfin utiliser les commandes de STAFF"+
+          "- **Fix des annonces de shorts (Vraiment une petite bêtises à tout niquer)"+
+          "- **Fix des annonces Mardi Zizi (Un caractère anti slash était ajouter qui est innexploitable) -> Désormais les MZ seront nomme soit `Mardi Z#zi` ou `Mardi Zlzi`\n\n"+
+
+          'Cette version du bot est donc normalement terminé à "100%" (Sauf correctif très mineur et ajout de nouveau stream type). Je travaille déjà sur la prochaine donc je suis toujours preneur de vos idées de commandes que vous souhaitez voir.'
 
 class command {
     constructor() {
@@ -22,7 +17,7 @@ class command {
 
     
     async execute(bot, interaction) {
-        if (interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+        if (interaction.member.permissions.has(PermissionsBitField.Flags.Administrators)) {
         var now = new Date();
         skipVideo = false;
         const SKIP = new EmbedBuilder()
