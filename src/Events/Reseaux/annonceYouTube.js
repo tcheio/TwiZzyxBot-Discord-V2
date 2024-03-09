d63830b9ca4325e3c5c93bc28862299ce9f80076
tcheio <113595151+tcheio@users.noreply.github.com>
2024-03-09 14:44:42 +0100

    Update retour short

--- src/Events/Reseaux/annonceYouTube.js
+++ src/Events/Reseaux/annonceYouTube.js
@@ -5,16 +5,21 @@ const youtube = require('../../Fonctions/YouTube');
 module.exports = async function annonceYouTube(bot,message) {
     if (message.channelId == config.channel.chainePrincipal){ //Channel #TwiZzyx/TwiZzyx-2
         msg = message.content;
-        if (description == null){
-            bot.channels.cache.get(config.channel.videos).send("# <:YouTube:748225835269488751>__**NOUVELLE VIDÉO**__<:YouTube:748225835269488751>\n\n\n"+msg+"\n\n||@everyone||");
+        if (youtube.rechercheCaract(msg)){
+            bot.channels.cache.get(config.channel.clip).send("# <:YouTube:748225835269488751>__**NOUVEAU SHORT**__<:YouTube:748225835269488751>\n\n\n"+msg+"\n\n||@everyone||");
         }
         else {
-            bot.channels.cache.get(config.channel.videos).send("# <:YouTube:748225835269488751>__**NOUVELLE VIDÉO**__<:YouTube:748225835269488751>\n\n\n"+youtube.createDesc(msg,description)+"\n\n||@everyone||");
-        }
+            if (description == null){
+                bot.channels.cache.get(config.channel.videos).send("# <:YouTube:748225835269488751>__**NOUVELLE VIDÉO**__<:YouTube:748225835269488751>\n\n\n"+msg+"\n\n||@everyone||");
+            }
+            else {
+                bot.channels.cache.get(config.channel.videos).send("# <:YouTube:748225835269488751>__**NOUVELLE VIDÉO**__<:YouTube:748225835269488751>\n\n\n"+youtube.createDesc(msg,description)+"\n\n||@everyone||");
+            }
         description = null;
         //log serveur
         console.log("Une vidéo a été publié à "+classique.temps());
         bot.channels.cache.get(config.channel.log).send("Une vidéo a été publié à "+classique.temps());
+        }
     }
 
     else if(message.channelId == config.channel.twizzyxReplay){ //Channel #Replay
