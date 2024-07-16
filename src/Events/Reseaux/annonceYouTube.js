e20557e1c0c1551d168df808ed23a952d3a7842b
Tcheio <113595151+tcheio@users.noreply.github.com>
2024-07-16 12:59:59 +0200

    Changement des channels test à channel réel

--- src/Events/Reseaux/annonceYouTube.js
+++ src/Events/Reseaux/annonceYouTube.js
@@ -3,17 +3,17 @@ const classique = require('../../Fonctions/Classique');
 const youtube = require('../../Fonctions/YouTube');
 
 module.exports = async function annonceYouTube(bot,message) {
-    if (message.channelId == config.channel.envoie){ //Channel #TwiZzyx/TwiZzyx-2
+    if (message.channelId == config.channel.chainePrincipal){ //Channel #TwiZzyx/TwiZzyx-2
         msg = message.content;
         if (youtube.rechercheCaract(msg)){
-            bot.channels.cache.get(config.channel.retour).send("# <:YouTube:748225835269488751>__**NOUVEAU SHORT**__<:YouTube:748225835269488751>\n\n\n"+msg+"\n\n||<@&1014452932713922610>||");
+            bot.channels.cache.get(config.channel.clip).send("# <:YouTube:748225835269488751>__**NOUVEAU SHORT**__<:YouTube:748225835269488751>\n\n\n"+msg+"\n\n||<@&1014452932713922610>||");
         }
         else {
-            bot.channels.cache.get(config.channel.retour).send("# <:YouTube:748225835269488751>__**NOUVELLE VIDÉO**__<:YouTube:748225835269488751>\n\n\n"+youtube.createDesc(msg,description)+"\n\n||@everyone||");
+            bot.channels.cache.get(config.channel.video).send("# <:YouTube:748225835269488751>__**NOUVELLE VIDÉO**__<:YouTube:748225835269488751>\n\n\n"+youtube.createDesc(msg,description)+"\n\n||@everyone||");
             description = null;
             //log serveur
             console.log("Une vidéo a été publié à "+classique.temps());
-            bot.channels.cache.get(config.channel.logTest).send("Une vidéo a été publié à "+classique.temps());
+            bot.channels.cache.get(config.channel.log).send("Une vidéo a été publié à "+classique.temps());
         }
     }
 
