3280d90a860e2361ca3eb93171108072dc8a69cb
techio <thomas.cheio0105@gmail.com>
2025-03-21 18:13:46 +0100

    Update Discord JS error

--- src/Events/Reseaux/annonceYouTube.js
+++ src/Events/Reseaux/annonceYouTube.js
@@ -6,22 +6,22 @@ module.exports = async function annonceYouTube(bot,message) {
     if (message.channelId == config.channel.chainePrincipal){ //Channel #TwiZzyx/TwiZzyx-2
         msg = message.content;
         if (youtube.rechercheCaract(msg)){
-            bot.channels.cache.get(config.channel.clip).send("# <:YouTube:748225835269488751>__**NOUVEAU SHORT**__<:YouTube:748225835269488751>\n\n\n"+youtube.createDesc(msg,description)+"\n\n||<@&1014452932713922610>||");
+            bot.channels.cache.get(config.channel.channelTampons).send("# <:YouTube:748225835269488751>__**NOUVEAU SHORT**__<:YouTube:748225835269488751>\n\n\n"+youtube.createDesc(msg,description)+"\n\n||<@&1014452932713922610>||");
         }
         else {
-            bot.channels.cache.get(config.channel.videos).send("# <:YouTube:748225835269488751>__**NOUVELLE VIDÉO**__<:YouTube:748225835269488751>\n\n\n"+youtube.createDesc(msg,description)+"\n\n||@everyone||");
+            bot.channels.cache.get(config.channel.channelTampons).send("# <:YouTube:748225835269488751>__**NOUVELLE VIDÉO**__<:YouTube:748225835269488751>\n\n\n"+youtube.createDesc(msg,description)+"\n\n||@everyone||");
             description = null;
             //log serveur
             console.log("Une vidéo a été publié à "+classique.temps());
-            bot.channels.cache.get(config.channel.log).send("Une vidéo a été publié à "+classique.temps());
+            bot.channels.cache.get(config.channel.channelTampons).send("Une vidéo a été publié à "+classique.temps());
         }
     }
 
     else if(message.channelId == config.channel.twizzyxReplay){ //Channel #Replay
         msg = message.content;
-        bot.channels.cache.get(config.channel.replay).send("# <:YouTubeBleu:1018805788090839061>__**NOUVEAU REPLAY**__<:YouTubeBleu:1018805788090839061>\n\n\n"+msg);
+        bot.channels.cache.get(config.channel.channelTampons).send("# <:YouTubeBleu:1018805788090839061>__**NOUVEAU REPLAY**__<:YouTubeBleu:1018805788090839061>\n\n\n"+msg);
         console.log("Un replay a été publié à "+classique.temps());
-        bot.channels.cache.get(config.channel.log).send("Une vidéo a été publié à "+classique.temps());
+        bot.channels.cache.get(config.channel.channelTampons).send("Une vidéo a été publié à "+classique.temps());
     }
 
 };
